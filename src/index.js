bac846a825ee37f85bd271a2ded2a0e0e3cb27d1
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-19 19:27:58 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkStr = bracketsConfig.flat();
+    let deufaltStr = str.split(''); 
+
+    console.log('deufakt string: ' + deufaltStr);
+    console.log('check string: ' + checkStr);
+    let count = 0
+        
+    for (let i = 0; i < deufaltStr.length; i++) {
+        if (deufaltStr[i] === checkStr[i]) {
+            count++;
+        } else {
+            count *= 0;
+        } 
+    }
+
+    if (count > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
