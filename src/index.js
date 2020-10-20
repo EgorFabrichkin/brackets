131f2388da19058984a11f45ab4d5cf03fbf1573
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-21 00:13:01 +0300

    fixed bags

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-    let checkStr = bracketsConfig.flat();
-    let deufaltStr = str.split(''); 
+    let checkedStr = [], sum = [], index = -1;
 
-    console.log('deufakt string: ' + deufaltStr);
-    console.log('check string: ' + checkStr);
-    let count = 0
-        
-    for (let i = 0; i < deufaltStr.length; i++) {
-        if (deufaltStr[i] === checkStr[i]) {
-            count++;
-        } else {
-            count *= 0;
-        } 
-    }
-
-    if (count > 0) {
+    if (str.length === 0) {
         return true;
     } else {
-        return false;
+        for (let i = 0; i < str.length; i++) {
+            let num = brackets(str[i], bracketsConfig);    
+            
+            if (index < 0) {
+                index = 0;
+                checkedStr[index] = num;
+                sum[index] = 0;
+            } else {
+                if (num != num[index]) {
+                    ++index;
+                    checkedStr[index] = num;
+                    sum[index] = 0;
+                }
+            }
+    
+            let start = bracketsConfig[checkedStr[index]][0], finish = bracketsConfig[checkedStr[index]][1];
+    
+            if (start != finish) {
+                if (c == open) {
+                    ++sum[index];
+                } else {
+                    --sum[index];
+                }
+            } else {
+                sum[index] = sum[index] == 0 ? 1 : 0;
+            }
+            if (sum[index] === 0) {
+                --index;
+            }
+
+        if (sum[index] < 0) {
+            return false
+        }    
+        return index == -1;
+        }
     }
+}
+
+function brackets(cutStr, bracketsConfig) {
+    let count = 0;
+    while((bracketsConfig[count][0] != cutStr) && (bracketsConfig[count][1] != cutStr)) {
+        count++;
+    }
+    return count;
 }
